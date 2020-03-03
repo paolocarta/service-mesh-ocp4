d724f18f78183f91806a2dbd03f723a6bb61a5c8
Paolo Carta <pcarta@redhat.com>
2020-03-03 18:15:04 +0100

    conditional git clone

--- demo-app-installation.sh
+++ demo-app-installation.sh
@@ -1,9 +1,16 @@
-git clone https://github.com/paolocarta/istio-workshop.git
+if [ ! -d "istio-workshop" ] 
+then
+    echo "Directory istio-workshop DOES NOT exist, cloning repo" 
+    git clone https://github.com/paolocarta/istio-workshop.git
+fi
+
 cd istio-workshop
 git checkout ocp4
 
 oc project istio-demo
 oc apply -f kubernetes-v1/permissive/
+
+cd ..
 # oc apply -f kubernetes-v2/permissive/
 
 
