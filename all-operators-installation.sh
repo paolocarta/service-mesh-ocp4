c250597b517df4cc4190a93bead18aac066ae4bc
Paolo Carta <pcarta@redhat.com>
2020-03-04 11:29:39 +0100

    improving ops script

--- all-operators-installation.sh
+++ all-operators-installation.sh
@@ -1,7 +1,11 @@
 chmod 777 operators/service-mesh-op-installation.sh
 chmod 777 operators/prerequisites-installation.sh
 
-./operators/prerequisites-installation.sh
+cd operators
+
+./prerequisites-installation.sh
 sleep 2
-./operators/service-mesh-op-installation.sh
+./service-mesh-op-installation.sh
 sleep 1
+
+cd ..
