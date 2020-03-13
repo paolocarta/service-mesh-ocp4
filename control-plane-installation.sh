174fc0350c6b6a98fa5b4214a1fefbc805d2128d
Paolo Carta <pcarta@redhat.com>
2020-03-13 18:21:09 +0100

    updating installation

--- control-plane-installation.sh
+++ control-plane-installation.sh
@@ -1,9 +1,10 @@
 oc new-project istio-system
+sleep 2
 oc apply -n istio-system -f control-plane/istio-installation.yaml
 
 sleep 1
 
-oc new-project istio-demo
+oc new-project istio-demo-app
 
 sleep 1
 
