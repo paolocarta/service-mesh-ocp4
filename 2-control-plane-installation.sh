bcf3f4191298705a9afa6c70a1dcec596147e8f3
Paolo Carta <ing.pcarta@gmail.com>
2020-06-22 17:20:06 +0200

    adding multitenant installations with env var

--- 2-control-plane-installation.sh
+++ 2-control-plane-installation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export DEMO_APP_PROJECT_NAME=istio-your-name
+export DEMO_APP_PROJECT_NAME=istio-paolocarta
 
 oc new-project $DEMO_APP_PROJECT_NAME
 sleep 2
@@ -13,4 +13,5 @@ oc new-project $DEMO_APP_PROJECT_NAME-demo-app
 
 sleep 1
 
+sed "s/REPLACEME/$DEMO_APP_PROJECT_NAME-demo-app/g" control-plane/servicemeshmemberroll-default.input.yaml > control-plane/servicemeshmemberroll-default.yaml
 oc apply -n $DEMO_APP_PROJECT_NAME -f control-plane/servicemeshmemberroll-default.yaml
